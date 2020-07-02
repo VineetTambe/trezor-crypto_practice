2dcaf0daccbd83c814e6b79e1320c5b7a246dc27
Vineet <52542471+VineetTambe@users.noreply.github.com>
2020-07-02 18:27:00 +0530

    Update main_01_07_2020.c

--- main_01_07_2020.c
+++ main_01_07_2020.c
@@ -87,9 +87,9 @@ int main()
     const char *received_unsigned_txn_string = "02000000021245fe7c5455b43e73743d83ccb5587303586a4e9a5b8f56a3eb08593624bb02000000001976a914d46d05e6ac27683aa5d63a6efc44969798acf13688acfdffffff1245fe7c5455b43e73743d83ccb5587303586a4e9a5b8f56a3eb08593624bb02010000001976a914dacc24d8b195ce046a40caedd5e2e649beee4e3388acfdffffff01f4ff0000000000001976a9142d77ece155f6b80dcab97a373834543e4b70b3e988ac84431a0001000000";
     
     char *test_str = "46a988ac";
-    uint8_t byte_array[360/2];
+    uint8_t byte_array[sizeof(received_unsigned_txn_string)/2];
 
-    hex_string_to_byte_array(received_unsigned_txn_string,360,&byte_array[0]);
+    hex_string_to_byte_array(received_unsigned_txn_string,sizeof(received_unsigned_txn_string),&byte_array[0]);
     printf("\n");
     for(i=0;i<360/2;i++)
     	printf("%d\t",byte_array[i]);
@@ -127,14 +127,20 @@ void hex_string_to_byte_array(const char *hex_string, uint32_t string_length, ui
 		byte_array[i] =  hex_to_dec(hex_string[i]);*/
 }
 
+
 void byte_array_to_unsigned_txn(uint8_t *btc_unsigned_txn_byte_array, unsigned_txn *unsigned_txn_ptr){
 	int i=0;
 	int j=0;
+	uint32_t len;
 	
 	for(;i<4;i++)
 		unsigned_txn_ptr->network_version[i] = btc_unsigned_txn_byte_array[i];
 	
 	unsigned_txn_ptr->input_count[0] = btc_unsigned_txn_byte_array[i++];
+	
+	len = (*unsigned_txn_ptr->input_count)*sizeof(unsigned_txn_input); 
+    unsigned_txn_ptr->input = (unsigned_txn_input*)malloc(len);
+	
 	
 	for(j=0;j<32;j++)
 		unsigned_txn_ptr->input->previous_txn_hash[j] = btc_unsigned_txn_byte_array[i+j];
@@ -155,6 +161,9 @@ void byte_array_to_unsigned_txn(uint8_t *btc_unsigned_txn_byte_array, unsigned_t
 	i+=j;
 	
 	unsigned_txn_ptr->output_count[0] = btc_unsigned_txn_byte_array[i++];
+	
+	len = (*unsigned_txn_ptr->output_count)*sizeof(txn_output); 
+      unsigned_txn_ptr->output = (txn_output*)malloc(len);
 	
 	for(j=0;j<8;j++)
 		unsigned_txn_ptr->output->value[j] = btc_unsigned_txn_byte_array[i+j];
